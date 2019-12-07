0a4feff785085afe127d61d8d3bb7ec9be421ee5
steveespinoza <steveespinoza@localhost>
2019-12-06 20:14:24 -0600

    Avance Steve y Kobe brayan

---
+++ src/Logic/Players.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kobem
+ */
+public class Players {
+    
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
